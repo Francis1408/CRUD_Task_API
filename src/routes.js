65230ae5cf61543762c2d5365c1f33181252d4f4
Francisco <franciscoabreu1408@gmail.com>
2023-09-15 09:45:49 -0300

    POST Request done

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 import { randomUUID } from 'node:crypto'
-import { Database } from './database'
-import { buildRoutePath } from './utils/build-route-path'
+import { Database } from './database.js'
+import { buildRoutePath } from './utils/build-route-path.js'
 
 const database = new Database()
 
@@ -13,8 +13,8 @@ export const routes = [
         handler: (req, res) => {
             const {title, description} = req.body
             const date = new Date()
-            const created_at = `${('0' + date.getDate()).slice(-2)}/${('0' + (date.getMonth()+1)).slice(-2)}/${date.getFullYear()} 
-                                at ${date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds()}`
+            const created_at = `${('0' + date.getDate()).slice(-2)}/${('0' + (date.getMonth()+1)).slice(-2)}/${date.getFullYear()} at ${date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds()}`
+            const updated_at = created_at
 
             const task = {
                 id: randomUUID(),
@@ -22,10 +22,14 @@ export const routes = [
                 description,
                 completed_at: null,
                 created_at,
+                updated_at  
                 
             }
-        }
 
-    }
+            database.insert('tasks', task)
+
+            return res.writeHead(201).end()
+        }
+    },
 
 ]
