58ed1eb43f953af6130bc055a1f681a1962483a9
Francisco <franciscoabreu1408@gmail.com>
2023-09-15 10:18:13 -0300

    Implementing PUT request

--- src/database.js
+++ src/database.js
@@ -31,4 +31,14 @@ export class Database {
 
         return data
     }
+
+    update(table, id, data) {
+        const rowIndex = this.#database[table].findIndex(row => row.id === id)
+
+        if(rowIndex > -1) {
+            const updated_at = `${('0' + date.getDate()).slice(-2)}/${('0' + (date.getMonth()+1)).slice(-2)}/${date.getFullYear()} at ${date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds()}`
+            this.#database[table][rowIndex] = { id, ...data, updated_at }
+            this.#persist()
+        }
+    }
 }
